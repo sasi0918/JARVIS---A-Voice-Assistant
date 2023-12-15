8952697cde676fe6e126b17478686e7894c62870
sasidhar1809 <149456106+sasidhar1809@users.noreply.github.com>
2023-12-16 00:59:04 +0530

    Add files via upload

---
+++ NEW JARVIS/MusicPlayer.py
@@ -0,0 +1,28 @@
+#import pygame
+#import threading
+#import time
+#
+#def play_music(file_path, stop_flag):
+#
+#init()
+#music.load(file_path)
+#music.play()
+#
+#mixer.music.get_busy() and not stop_flag.is_set():
+#me.Clock().tick(10)
+#
+#music.stop()
+#
+#op_flag):
+#Enter to stop the music...")
+#()
+#
+#ile_path):
+#hreading.Event()
+#= threading.Thread(target=play_music, args=(file_path, stop_flag))
+#start()
+#
+# threading.Thread(target=stop_music, args=(stop_flag,))
+#tart()
+
+# For testing purposes
